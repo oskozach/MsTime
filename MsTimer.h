f2b0326642686d66b7a624d73153bd566f9d07d7
oskozach <osko.zach@gmail.com>
2025-02-17 20:34:31 +0200

    Refactored the MsTimer class

--- MsTimer.h
+++ MsTimer.h
@@ -5,13 +5,20 @@
 class MsTimer {
     private:
         uint32_t _timeout;
-        uint32_t _msLastReset;
+        uint32_t _numRepeats;
+        uint32_t _ms;
+        uint32_t _cnt;
+        bool _state;
     public:
         MsTimer();
-        MsTimer(uint32_t timeout);
-        bool isReady();
-        void setTimeout(uint32_t timeout);
+        MsTimer(uint32_t timeout, uint32_t numRepeats = 0);
+        virtual ~MsTimer();
+        void begin();
         void reset();
-        void restart();
+        virtual void start();
+        virtual void stop();
+        virtual bool tick();
+        void setTimeout(uint32_t timeout);
+        void setNumRepeats(uint32_t num);
 };
 #endif
